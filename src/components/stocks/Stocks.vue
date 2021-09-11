f6cd63b30221f594eaac224862834c67bcb0e947
Gift1rebecca <oluwasegungift@gmail.com>
2021-09-11 03:43:01 +0100

    creating stock components

---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <app-stock v-for="stock in stocks"</app-stock>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                stocks: [
+                    { id: 1, name: 'BMW', price: 110 },
+                    { id: 2, name: 'Goggle', price: 200 },
+                    { id: 3, name: 'Apple', price: 250 },
+                    { id: 4, name: 'Twitter', price: 8 },
+                ]
+            }
+        },
+        components: {
+            appStock: Stock
+        }
+    }
+</script>
